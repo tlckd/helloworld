9ad4789b596cdddf52db7773ca964fd5b31c4c69
tlckd <104331037+tlckd@users.noreply.github.com>
2022-04-26 10:38:35 +0900

    Update HelloWorld.java

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("HelloWorld!!!!! change");
+		System.out.print("HelloWorld!!!!! 깃사이트에서 수정 change");
 	}
 
 }
